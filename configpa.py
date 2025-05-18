eabe3c8ac92806fe36df485ada5b1e983c3b5c83
PCM11 <phumitshidi@gmail.com>
2025-05-18 18:57:07 +0100

    amended config

---
+++ configpa.py
@@ -0,0 +1,8 @@
+# For configuration
+
+db_config = {
+    'host': 'PCM11.mysql.pythonanywhere-services.com',
+    'user': 'PCM11',
+    'password': 'rootanywhere',
+    'database': 'PCM11$grocery'
+}
